995de2a84949fe610f38403d750189a5eef35638
Dmitry White <dmitry.b.white@gmail.com>
2021-10-13 16:01:54 +0100

    Refactor setup files

--- first-container/app.js
+++ first-container/app.js
@@ -1,24 +1,29 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var app = express();
-var os = require("os");
-var morgan  = require('morgan');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const os = require("os");
+const morgan = require("morgan");
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.static('static'));
-app.use(morgan('combined'));
+const port = process.env.PORT || 8080;
+const message = process.env.MESSAGE || "Have a blast learning Docker!";
+const hostname = os.hostname();
 
-var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "Have a blast learning Docker!";
+const app = express();
 
-app.get('/', function (req, res) {
-    res.render('home', {
-      message: message,
-      hostName: os.hostname()
-    });
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("static"));
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  const data = {
+    message: message,
+    hostName: hostname,
+  };
+
+  res.render("home", data);
 });
 
-app.listen(port, function () {
-  console.log("Listening on: http://%s:%s", os.hostname(), port);
+app.listen(port, () => {
+  console.log(`Listening on: http://${hostname}:${port}`);
 });
