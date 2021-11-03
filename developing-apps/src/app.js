2d6e1803b5da34a6182c238e3bb0e0f4276a9651
Dmitry White <dmitry.b.white@gmail.com>
2021-11-03 20:35:21 +0000

    Developing Apps: Add volume and nodemon support

--- developing-apps/src/app.js
+++ developing-apps/src/app.js
@@ -2,17 +2,22 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const os = require("os");
 const morgan = require("morgan");
+const path = require("path");
 
 const port = process.env.PORT || 8080;
 const message = process.env.MESSAGE || "Have a blast learning Docker!";
 const hostname = os.hostname();
+const viewsPath = path.join(__dirname, "views");
+const staticPath = path.join(__dirname, "static");
 
 const app = express();
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.set("views", viewsPath);
 
-app.use(express.static("static"));
+app.use(express.static(staticPath));
+
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
